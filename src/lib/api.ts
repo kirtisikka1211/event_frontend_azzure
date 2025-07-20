986971e6b9750fa356d52bf06ebf1979693fb9dd
Kirti Sikka <116485331+kirtisikka1211@users.noreply.github.com>
2025-07-20 15:54:52 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 // const API_BASE_URL = 'https://event-backend-1-n491.onrender.com/api';
-const API_BASE_URL = 'http://localhost:5004/api';
+const API_BASE_URL = 'https://eventhh-afcpamdvfxfwfhcj.westindia-01.azurewebsites.net/api';
+
 class ApiClient {
   private token: string | null = null;
 
